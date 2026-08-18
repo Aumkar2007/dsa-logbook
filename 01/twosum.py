5ef72c3fff4ad3f92bafa92fd036cfeb7afc162f
OmkarM <omkarsuchitmundada2007@gmail.com>
2026-08-19 00:55:50 +0530

    Update twosum.py

--- 01/twosum.py
+++ 01/twosum.py
@@ -1,1 +1,16 @@
-helo
+# Problem Link: https://leetcode.com
+# Pattern: Two Pointers / Hash Map
+# Time Complexity: O(N)
+# Space Complexity: O(N)
+
+def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        n=len(numbers)
+        i=0
+        j=n-1
+        while i<j:
+            if numbers[i]+numbers[j]==target:
+                return [i+1,j+1]
+            elif numbers[i]+numbers[j]>target:
+                j-=1
+            elif numbers[i]+numbers[j]<target:
+                i+=1
